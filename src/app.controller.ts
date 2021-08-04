1ab8e4e43b4d224f8652ea947500f8a02c66ba0a
Khaalid <khaalidsubaan@gmail.com>
2021-08-04 23:57:07 +0800

    initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+
+  @Get()
+  getHello(): string {
+    return 'hello world'
+  }
+}
